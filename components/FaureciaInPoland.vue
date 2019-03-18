3ce4a12512e0193b7112a9d620f8eceeb447333c
ArkejGit <robert.krawczyk92@gmail.com>
2019-03-18 18:40:28 +0100

    Modify component to be more 'vue'

--- components/FaureciaInPoland.vue
+++ components/FaureciaInPoland.vue
@@ -4,22 +4,11 @@
 			<div class="row">
 				<div class="d-none d-lg-block col-lg-3">
 					<ul data-aos="fade">
-						<li data-city="grojec">Grójec</li>
-						<li data-city="grojec">Grójec i Wałbrzych R&D</li>
-						<li data-city="gorzow">Gorzów Wielkopolski</li>
-						<li data-city="legnica">Legnica</li>
-						<li data-city="jelcz">Jelcz-Laskowice</li>
-						<li data-city="walbrzych">Wałbrzych</li>
-						<li data-city="lodz">Łódź IT Support</li>
+						<li v-for="branch in branches" @mouseover="showCities(branch.cities)" @mouseleave="hideCities(branch.cities)">{{ branch.name }}</li>
 					</ul>
 					<div class="poland-dotted-container" data-aos="fade">
 						<img class="poland-dotted" src="/poland-dotted.jpg" alt="">
-						<div class="dot" data-top="51.8%" data-left="67.3%" data-city="grojec"></div>
-						<div class="dot" data-top="30.05%" data-left="12%" data-city="gorzow"></div>
-						<div class="dot" data-top="64.5%" data-left="23.7%" data-city="legnica"></div>
-						<div class="dot" data-top="69%" data-left="32%" data-city="jelcz"></div>
-						<div class="dot" data-top="73.2%" data-left="23.7%" data-city="walbrzych"></div>
-						<div class="dot" data-top="56%" data-left="52%" data-city="lodz"></div>
+						<div v-for="city in cities" :class="{'dot-show': city.show}" class="dot" :style="{ top: `${city.top}%`, left: `${city.left}%` }"></div>
 					</div>
 				</div>
 				<div class="image-container col-12 col-lg-9">
@@ -36,24 +25,90 @@
 
 <script>
 export default {
-	mounted: function () {
-		const dots = document.querySelectorAll('.dot')
-		dots.forEach((dot) => {
-			dot.style.top = dot.dataset.top
-			dot.style.left = dot.dataset.left
-		})
-
-		const lis = document.querySelectorAll('li[data-city]')
-		lis.forEach((li) => {
-			const city = li.dataset.city
-			const dot = document.querySelector(`.dot[data-city=${city}]`)
-			li.addEventListener('mouseenter', () => {
-				dot.style.opacity = 0.7
+	data() {
+		return {
+			grojec: false,
+			branches: [
+				{
+					name: 'Grójec',
+					cities: ['grojec']
+				},
+				{
+					name: 'Grójec i Wałbrzych R&D',
+					cities: ['grojec', 'walbrzych']
+				},
+				{
+					name: 'Gorzów Wielkopolski',
+					cities: ['gorzow']
+				},
+				{
+					name: 'Legnica',
+					cities: ['legnica']
+				},
+				{
+					name: 'Jelcz-Laskowice',
+					cities: ['jelcz']
+				},
+				{
+					name: 'Wałbrzych',
+					cities: ['walbrzych']
+				},
+				{
+					name: 'Łódź IT Support',
+					cities: ['lodz']
+				}
+			],
+			cities: {
+				grojec: {
+					name: 'Grójec',
+					top: '51.8',
+					left: '67.3',
+					show: false
+				},
+				gorzow: {
+					name: 'Gorzów Wielkopolski',
+					top: '30.05',
+					left: '12',
+					show: false
+				},
+				legnica: {
+					name: 'Legnica',
+					top: '64.5',
+					left: '23.7',
+					show: false
+				},
+				jelcz: {
+					name: 'Jelcz-Laskowice',
+					top: '69',
+					left: '32',
+					show: false
+				},
+				walbrzych: {
+					name: 'Wałbrzych',
+					top: '73.2',
+					left: '23.7',
+					show: false
+				},
+				lodz: {
+					name: 'Łódź',
+					top: '56',
+					left: '52',
+					show: false
+				}
+			}
+		}
+	},
+	methods: {
+		showCities(cities) {
+			cities.forEach((city) => {
+				this.cities[city].show = true
 			})
-			li.addEventListener('mouseleave', () => {
-				dot.style.opacity = 0
+		},
+		hideCities(cities) {
+			cities.forEach((city) => {
+				this.cities[city].show = false
 			})
-		})
+		}
 	}
 }
 </script>
@@ -110,4 +165,6 @@ export default {
 		opacity: 0
 		transition: opacity .5s ease
 		animation: pulse 1s infinite
+	.dot-show
+		opacity: .7
 </style>
